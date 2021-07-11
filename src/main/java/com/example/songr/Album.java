ad3e0ac31682c8d03b320fe9e4ed15f85c1b2225
Mohammad jaradat <morjaradat66@gmail.com>
2021-07-11 19:43:04 +0300

    the basic server properties

---
+++ src/main/java/com/example/songr/Album.java
@@ -0,0 +1,62 @@
+package com.example.songr;
+
+public class Album{
+    private String title;
+    private String artist;
+    private String songCount;
+    private String length;
+    private String imageUrl ;
+
+    public Album(String title, String artist, String songCount, String length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public Album(){
+
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setSongCount(String songCount) {
+        this.songCount = songCount;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getSongCount() {
+        return songCount;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+}
--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.songr;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SongrApplicationTests {
+	Album test= new Album();
+	Album egyptRomance= new Album("egyptRomance","Mohamed Hamaki","58 songs","3 hr 44 min","https://i.scdn.co/image/ab67706f000000039e65e57c167ef4f9487b9093");
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public  void testEmptyConstructor(){
+		Assertions.assertNull(test.getArtist());
+	}
+	@Test
+	public  void testConstructor(){
+		Assertions.assertNotNull(egyptRomance.getArtist());
+		Assertions.assertEquals("egyptRomance",egyptRomance.getTitle());
+		Assertions.assertEquals("Mohamed Hamaki",egyptRomance.getArtist());
+		Assertions.assertEquals("58 songs",egyptRomance.getSongCount());
+		Assertions.assertEquals("3 hr 44 min",egyptRomance.getLength());
+		Assertions.assertEquals("https://i.scdn.co/image/ab67706f000000039e65e57c167ef4f9487b9093",egyptRomance.getImageUrl());
 	}
 
 }
