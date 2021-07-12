a16d4beb8f89c7cf26582eec30977f1c4a20bb61
Mohammad jaradat <morjaradat66@gmail.com>
2021-07-12 21:42:46 +0300

    add post method

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 - the Albums page have a list of albums with some style
 - the Hello page contain hello world by default, and you can change it by add query word in Url like http://localhost:8080/hello?word=mmm 
 - the capitalize page contain hello DEFAULT by default, and you can change it by change the word  default in Url, and the return will be all in upperCase like http://localhost:8080/capitalize/default 
+
+- you can run the server by ./gradlew runBoot
--- src/main/java/com/example/songr/Album.java
+++ src/main/java/com/example/songr/Album.java
@@ -1,6 +1,15 @@
 package com.example.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String title;
     private String artist;
     private String songCount;
@@ -15,8 +24,11 @@ public class Album{
         this.imageUrl = imageUrl;
     }
 
-    public Album(){
+    public Album() {
+    }
 
+    public long getId() {
+        return id;
     }
 
     public void setTitle(String title) {
